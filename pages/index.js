85507e069b78777853c99df4d29273c3eba0a4c1
manas5996 <manas5996@gmail.com>
2023-03-22 08:18:28 +0530

    coded frontend for delete database

--- pages/index.js
+++ pages/index.js
@@ -19,7 +19,7 @@ import "react-toastify/dist/ReactToastify.css";
 import { databsesGetAction } from "@/frontend/redux/actions/databaseActions";
 
 export default function Home() {
-  const [deleteId, setDeleteId] = useState("");
+  const [deleteModelName, setDeleteModelName] = useState("");
   const [visible, setVisible] = useState(false);
 
   const dispatch = useDispatch();
@@ -36,19 +36,27 @@ export default function Home() {
     }
   }, [dispatch, databasesget]);
 
-  const addEmployeehandler = () => {
+  const createDBhandler = () => {
     setVisible(!visible);
   };
 
+  const viewDBhandler = () => {
+    setVisible(!visible);
+  };
+
+  const deleteDBhandler = (dbName) => {
+    setDeleteModelName(dbName);
+  };
+
   const deletehandler = () => {
-    if (deleteId) {
-      dispatch(employeeDeleteAction(deleteId));
-      setDeleteId("");
+    if (deleteModelName) {
+      dispatch(employeeDeleteAction(deleteModelName));
+      setDeleteModelName("");
     }
   };
 
   const cancelhandler = () => {
-    setDeleteId("");
+    setDeleteModelName("");
   };
 
   return (
@@ -76,7 +84,7 @@ export default function Home() {
             <div className="container mx-auto flex justify-between py-5 border-b">
               <div className="left flex gap-3">
                 <button
-                  onClick={addEmployeehandler}
+                  onClick={createDBhandler}
                   className="flex bg-indigo-500 text-white px-4 py-2 border rounded-md hover:bg-grary-50 hover:border-indigo-500 hover:text-gray-800"
                 >
                   Create Database{" "}
@@ -85,7 +93,7 @@ export default function Home() {
                   </span>
                 </button>
               </div>
-              {deleteId ? (
+              {deleteModelName ? (
                 DeleteComponent({ deletehandler, cancelhandler })
               ) : (
                 <></>
@@ -102,36 +110,37 @@ export default function Home() {
             <div className="py-5 grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4">
               {databasesget ? (
                 databasesget.models.map((model) => (
-                  <>
-                    <div className="bg-white p-4 rounded-md shadow-md">
-                      <div className="flex justify-between">
-                        <div className="p-2">
-                          <h6 className="text-xl  text-center font-bold">
-                            Database Name
-                          </h6>
-                          <span>{model}</span>
-                        </div>
-                        <div className="p-2">
-                          <button
-                            onClick={addEmployeehandler}
-                            className="my-2 flex bg-yellow-500 text-white px-4 py-2 border rounded-md hover:bg-grary-50 hover:border-indigo-500 hover:text-gray-800"
-                          >
-                            <span className="px-1">
-                              <BsDatabase size={23}></BsDatabase>
-                            </span>
-                          </button>
-                          <button
-                            onClick={addEmployeehandler}
-                            className="flex bg-red-500 text-white px-4 py-2 border rounded-md hover:bg-grary-50 hover:border-indigo-500 hover:text-gray-800"
-                          >
-                            <span className="px-1">
-                              <BsDatabase size={23}></BsDatabase>
-                            </span>
-                          </button>
-                        </div>
+                  <div
+                    key={model}
+                    className="bg-white p-4 rounded-md shadow-md"
+                  >
+                    <div className="flex justify-between">
+                      <div className="p-2">
+                        <h6 className="text-xl  text-center font-bold">
+                          Database Name
+                        </h6>
+                        <span>{model}</span>
+                      </div>
+                      <div className="p-2">
+                        <button
+                          onClick={viewDBhandler}
+                          className="my-2 flex bg-yellow-500 text-white px-4 py-2 border rounded-md hover:bg-grary-50 hover:border-yellow-500 hover:text-gray-800"
+                        >
+                          <span className="px-1">
+                            <BsDatabase size={23}></BsDatabase>
+                          </span>
+                        </button>
+                        <button
+                          onClick={() => deleteDBhandler(model)}
+                          className="flex bg-red-500 text-white px-4 py-2 border rounded-md hover:bg-grary-50 hover:border-red-500 hover:text-gray-800"
+                        >
+                          <span className="px-1">
+                            <BsDatabase size={23}></BsDatabase>
+                          </span>
+                        </button>
                       </div>
                     </div>
-                  </>
+                  </div>
                 ))
               ) : (
                 <></>
