1e909d59d900f78cc886473b85be4e3f596ae8c9
manas5996 <manas5996@gmail.com>
2023-03-23 18:59:04 +0530

    coded db data delete controller

--- backend/controllers/modelsDataController.js
+++ backend/controllers/modelsDataController.js
@@ -138,3 +138,49 @@ export async function getDBData(req, res) {
     res.status(500).json({ message: `Failed to fetch models`, error: error });
   }
 }
+
+// DEL /api/modelApi/modelsData/[modeName] -> delete a model data from a database
+export async function deleteDBData(req, res) {
+  const { modelName } = req.query;
+  const { dbDataId } = req.body;
+  try {
+    if (!modelName)
+      return res
+        .status(400)
+        .json({ message: "Model name does not send through request!" });
+    // Get a list of available models in the database
+    const modelNames = await getModelNames();
+    if (modelNames.includes(modelName)) {
+      if (mongoose.connection.models[modelName]) {
+        console.log("Model already exists, deleting from cache...");
+        // Clear compiled model from cache
+        delete mongoose.connection.models[modelName];
+        // delete mongoose.connection.modelSchemas[modelName];
+      }
+      const filePath = path.join(schemaFolderPath, `${modelName}.json`);
+      const schemaFromStoredFile = JSON.parse(fs.readFileSync(filePath));
+      const modDBCustomSchema = await getSchemaForModel(schemaFromStoredFile);
+      const MyModelFetch = mongoose.model(modelName, modDBCustomSchema);
+      const foundDBData = await MyModelFetch.findById(dbDataId);
+      if (foundDBData) {
+        const removeResult = await foundDBData.remove();
+        return res.status(200).json({
+          message: `Model: ${modelName} found in database! and data removed successfully`,
+          model: modelName,
+          removeResult: removeResult,
+          found: true,
+        });
+      } else {
+        return res.status(404).json({ message: "Custom data not found" });
+      }
+    } else {
+      return res.status(404).json({
+        message: `Model: ${modelName} do not found!`,
+        model: modelName,
+        found: false,
+      });
+    }
+  } catch (error) {
+    return res.status(404).json({ error });
+  }
+}
--- pages/api/modelApi/modelsData/[modelName].js
+++ pages/api/modelApi/modelsData/[modelName].js
@@ -1,6 +1,7 @@
 import connectMongo from "@/backend/config/database/conn";
 import {
   createDBData,
+  deleteDBData,
   getDBData,
 } from "@/backend/controllers/modelsDataController";
 
@@ -19,10 +20,10 @@ export default async function handler(req, res) {
       // POST /api/modelApi/modelsData/[modeName] -> create data of a DB
       await createDBData(req, res);
       break;
-    // case "DELETE":
-    //   // DEL /api/modelApi/modelsReq/[modeName] -> delete a model from a database
-    //   await deleteModel(req, res);
-    //   break;
+    case "DELETE":
+      // DEL /api/modelApi/modelsData/[modeName] -> delete a model data from a database
+      await deleteDBData(req, res);
+      break;
     default:
       res.setHeader("Allow", ["GET", "POST", "PUT", "DELETE"]);
       res.status(405).end(`Method ${method} Not Allowd`);
